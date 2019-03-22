77ef1865407dbea183aa8294fd1226f835ca0245
Brianna Besinaiz <blbesinaiz@gmail.com>
2019-03-22 07:35:56 -0400

    "DB" Fix

---
+++ Portal3/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal3
+{
+    public class Database
+    {
+        //To implement this "database" you just need to have "using Portal3" at the top of the web form
+        //To call it you simply need to do "Database.test" or "Database.test_function();
+        public static string test;
+
+        public static void test_function()
+        {
+
+        }
+    }
+}
